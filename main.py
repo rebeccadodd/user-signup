e2d37e20b0635b1e4225d23705d067ca49bd6f16
doddy <rebecca.dodd@gmail.com>
2017-10-19 13:43:52 -0500

    Rewrote code and made it match the example given for the assignment

--- main.py
+++ main.py
@@ -1,45 +1,70 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=["POST"])
-def sign_up():
-    username = request.form['username']
-    password = request.form['password']
-    verify = request.form['verify']
-    email = request.form['email']
-    #We got the info from the form submitted by the user
-    username_error = ""
-    password_error = ""
-    verify_error = ""
-    email_error = ""
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template("signup.html")
 
-    if username == "" or " " in username or len(username) < 3 or len(username) > 20:
-        username_error = "Invalid username"
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        email = request.form['email']
+        error_username = ""
+        error_password = ""
+        error_verify = ""
+        error_email = ""
 
-    if password == "" or " " in password or len(password) < 3 or len(password) > 20:
-        password_error = "Invalid password"
+        if (username == ""
+                or " " in username
+                or len(username) < 3
+                or len(username) > 20):
+            error_username = "Please enter a valid username"
 
-    if verify != password:
-        verify_error = "Your passwords do not match"
+        if (password == ""
+                or " " in password
+                or len(password) < 3
+                or len(password) > 20):
+            error_password = "Please enter a valid password"
 
-    if email != "":
-        if "@" not in email or "." not in email or " " in email or len(email) < 3 or len(email) > 20:
-            email_error = "Invalid email"
+        if verify == "":
+            error_verify = "Please enter a valid password"
 
-    if email_error == "" and username_error == "" and verify_error == "" and password_error == "":
-        return render_template("welcome.html", username = username)  
-    else:
-        return render_template("index.html", username_error = username_error
-                                           , password_error = password_error
-                                           , verify_error = verify_error
-                                           , email_error = email_error
-                                           , username = username
-                                           , email = email)  
+        if verify != password:
+            error_verify = "Passwords do not match"
 
-@app.route("/")
+        if email != "":
+            if ('@' not in email
+                    or "." not in email
+                    or " " in email
+                    or len(email) < 3
+                    or len(email) > 20):
+                error_email = "Please enter a valid email address"
+
+        if (error_username == ""
+                and error_password == ""
+                and error_verify == ""
+                and error_email == ""):
+            return redirect("/welcome?username=" + username)
+        else:
+            return render_template("signup.html", error_username=error_username,
+                                                  error_password=error_password,
+                                                  error_verify=error_verify,
+                                                  error_email=error_email,
+                                                  username=username,
+                                                  email=email)
+
+@app.route("/welcome", methods=['GET'])
+def welcome():
+    username = request.args.get('username')
+    return render_template("welcome.html", username=username)
+
+@app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html")
+    return redirect("/signup")
 
-app.run()
+if __name__ == '__main__':
+    app.run()
